5ce7fcb07e7407b34576a598e6f7f9bb15b9285a
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-05 13:33:57 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map {|el| badge_maker(el)}
+end
+
+def assign_rooms(array)
+  rooms=[]
+  array.each_with_index do |name, i|
+    rooms.push("Hello, #{name}! You'll be assigned to room #{i+1}!")
+  end
+  rooms
+end
+
+def printer(array)
+  batch_badge_creator(array).each {|el| puts el}
+  assign_rooms(array).each {|el| puts el}
+end
